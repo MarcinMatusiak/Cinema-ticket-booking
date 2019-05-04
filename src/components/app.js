8a07c3e3826726a2ef69d692b7d60d0c879f4739
wojtich <wojtich@gmail.com>
2019-05-04 19:39:40 +0000

    Upload New File

---
+++ src/components/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import themoviedb from '../api/themoviedb';
+import SearchBar from './SearchBar';
+import MoviesList from './moviesList';
+
+class App extends React.Component {
+	state = { movies: [] };
+
+	async componentDidMount() {
+		const response = await themoviedb.get(
+			'/movie/popular?api_key=90c5cb06ac52c1395de20502caaa6a51&language=en-US&page=2&region=pl',
+			// to mozna zmienic za pomoca INTERCEPTORS
+			{}
+		);
+
+		this.setState({ movies: response.data.results });
+	}
+
+	render() {
+		return (
+			<div className="ui container">
+				<SearchBar />
+				<MoviesList movies={this.state.movies} />
+			</div>
+		);
+	}
+}
+
+export default App;
