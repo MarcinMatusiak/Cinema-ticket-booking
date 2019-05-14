e5d75371e5d29b9143b3130e439d971cb5c0d021
Dominika <matus.dominika@gmail.com>
2019-05-14 12:42:13 +0200

    component change

--- src/components/app.js
+++ src/components/app.js
@@ -12,22 +12,33 @@ class App extends React.Component {
 
 	async componentDidMount() {
 		const response = await themoviedb.get(
-			'/movie/popular?api_key=90c5cb06ac52c1395de20502caaa6a51&language=en-US&page=2&region=pl',
+			'https://api.themoviedb.org/3/movie/now_playing?api_key=90c5cb06ac52c1395de20502caaa6a51&language=en-US&page=1',
 			// to mozna zmienic za pomoca INTERCEPTORS
 			{}
 		);
 
 		this.setState({ movies: response.data.results });
-	}
+		console.log(this.state.movies)
+		if (this.state.movies[1].title.includes('Shazam'))
+		console.log("Tak");
+	};
 
 	onSubmitSearch = async (text) => {
-		const response = await themoviedb.get(
-			'/search/movie?api_key=90c5cb06ac52c1395de20502caaa6a51', {
-				params: {query: text}}
-		);
+		// const response = await themoviedb.get(
+		// 	'/search/movie?api_key=90c5cb06ac52c1395de20502caaa6a51', {
+		// 		params: {query: text}}
+		// );
 
-		this.setState({ movies: response.data.results });
-	}
+		const response = await this.state.movies.map((movie) => {
+		if(movie.title.includes(text))
+		{
+		    const url = 'https://image.tmdb.org/t/p/w300/';
+			return <img key={movie.id} src={`${url}${movie.poster_path}`} alt={movie.title} />;
+		}});
+		
+		console.log(text)
+		this.setState({ movies: response});
+	};
 
 	render() {
 		return (
